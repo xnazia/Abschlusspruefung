6234f88337d278f9a668d2e8380aa688d9cc2c6f
Nazia <wi21b122@technikum-wien.at>
2022-06-22 13:40:40 +0200

    Initial commit

---
+++ src/main/java/com/example/abschlusspruefung/AbschlusspruefungApplication.java
@@ -0,0 +1,13 @@
+package com.example.abschlusspruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlusspruefungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlusspruefungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/abschlusspruefung/AbschlusspruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abschlusspruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlusspruefungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
